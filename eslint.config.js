6d385854a9daf3af9d845dbaaa765d3602ee4390
Kyle Treubig <kyle@vimofthevine.com>
2025-11-02 22:14:43 +0000

    Configure import eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -5,11 +5,32 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
 
 export default defineConfig([
   globalIgnores(["dist"]),
+  importPlugin.flatConfigs.recommended,
+  {
+    files: ["eslint.config.js"],
+    rules: {
+      "import/no-unresolved": [
+        "error",
+        {
+          ignore: ["typescript-eslint", "eslint/config"]
+        }
+      ]
+    }
+  },
   {
     files: ["**/*.{ts,tsx}"],
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          project: "./tsconfig.json",
+        },
+      },
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,6 +41,24 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+    }
   },
   eslintConfigPrettier,
 ]);
