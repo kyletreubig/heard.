d05892b1c6885e676cd98276c50e2feafcd5ff8f
Kyle Treubig <kyle@vimofthevine.com>
2025-11-02 22:15:45 +0000

    Fix import order

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+
 import reactLogo from "./assets/react.svg";
+// eslint-disable-next-line import/no-unresolved
 import viteLogo from "/vite.svg";
 import "./App.css";
 
