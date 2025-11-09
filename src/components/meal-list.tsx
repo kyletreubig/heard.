743e0f965298a2dc53d024517cd7a5a849e2416e
Kyle Treubig <kyle@vimofthevine.com>
2025-11-09 15:03:26 +0000

    Support editing meal list on mobile

--- src/components/meal-list.tsx
+++ src/components/meal-list.tsx
@@ -99,19 +99,58 @@ export function MealList() {
         {meals?.map((meal) => (
           <Card key={meal.id}>
             <CardHeader>
-              <CardTitle className="text-lg">{meal.name}</CardTitle>
+              <CardTitle className="text-lg">
+                <EditableName
+                  name={meal.name}
+                  onChange={(newName) => updateMeal(meal.id, { name: newName })}
+                />
+              </CardTitle>
             </CardHeader>
             <CardContent className="space-y-4">
               <div>
                 <p className="not-prose text-sm text-muted-foreground">Date</p>
                 <p className="not-prose text-base font-medium">
-                  {meal.date.toLocaleString()}
+                  <EditableDate
+                    date={meal.date}
+                    onChange={(newDate) =>
+                      updateMeal(meal.id, { date: newDate })
+                    }
+                  />
                 </p>
               </div>
               <SelectMealButton meal={meal} />
             </CardContent>
           </Card>
         ))}
+
+        <Card>
+          <CardHeader>
+            <CardTitle>
+              <Input
+                placeholder="New meal"
+                {...form.register("name", { required: true })}
+              />
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <div>
+              <p className="not-prose text-sm text-muted-foreground">Date</p>
+              <p className="not-prose text-base font-medium">
+                <Input
+                  type="datetime-local"
+                  {...form.register("date", { required: true })}
+                />
+              </p>
+            </div>
+            <Button
+              className="w-full"
+              disabled={!form.formState.isDirty || !form.formState.isValid}
+              type="submit"
+            >
+              <Plus /> Add
+            </Button>
+          </CardContent>
+        </Card>
       </div>
     </form>
   );
