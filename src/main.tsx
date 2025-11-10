2fbc4754b9d5789c7e9f197b05c327e316544ab4
Kyle Treubig <kyle@vimofthevine.com>
2025-11-10 01:49:47 +0000

    Add error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
 
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary
+      fallbackRender={({ error }) => (
+        <div className="p-4 bg-red-100 text-red-800">
+          <h2 className="font-bold mb-2">Something went wrong:</h2>
+          <pre className="whitespace-pre-wrap">{error.message}</pre>
+        </div>
+      )}
+    >
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 );
