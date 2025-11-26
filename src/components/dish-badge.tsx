5e096c5d5fcf559840214733adab1ccd3e9acd7c
Kyle Treubig <kyle@vimofthevine.com>
2025-11-26 14:33:46 +0000

    Navigate to dish by clicking on badge

--- src/components/dish-badge.tsx
+++ src/components/dish-badge.tsx
@@ -1,6 +1,8 @@
 import { useMemo } from "react";
 
 import { useDish } from "@/api/dishes";
+import { useDishSelectionStore } from "@/stores/dish-selection";
+import { useDishTabSelectionStore } from "@/stores/dish-tab-selection";
 import { getColorFromString, getContrastingTextColor } from "@/utils/colors";
 
 import { Badge } from "./ui/badge";
@@ -13,5 +15,23 @@ export function DishBadge({ id }: { id: number }) {
     () => getContrastingTextColor(background),
     [background],
   );
-  return <Badge style={{ background, color }}>{text}</Badge>;
+
+  const setSelectedDish = useDishSelectionStore.use.setSelectedDish();
+  const setSelectedDishTab = useDishTabSelectionStore.use.setSelectedTab();
+  const handleClick = () => {
+    if (dish) {
+      setSelectedDishTab("steps");
+      setSelectedDish(dish);
+    }
+  };
+
+  return (
+    <Badge
+      className="hover:cursor-pointer"
+      onClick={handleClick}
+      style={{ background, color }}
+    >
+      {text}
+    </Badge>
+  );
 }
